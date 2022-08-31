9f5925149867bf6d36eca3d2b16dd4303a7e90ca
Max <hs2max@gmail.com>
2022-08-31 11:14:46 +0700

    add all

--- post.php
+++ post.php
@@ -9,9 +9,16 @@
 <body>
 <h1 style="text-align:center ;">Webbord KakKak</h1><hr>
 <div align="center">
+ต้องการกระทู้หมายเลข<?php echo $_GET['id'];?>
+<table style="border:2px solid black;width: 40%" align="center">
+<tr ><td style="text-align:left ;background-color: #6CD2FE;">กรอกข้อมูล</td></tr>
+<tr ><td style="text-align:center ;"><textarea name="message" rows="10"col="30">
+</textarea></td></tr>
+<tr><td style="text-align:center"><form><input type="submit" value="ส่งข้อความ"></form></td></tr>
 
-
-
+</table>
+<br>
+<a  href = "index.html">กลับไปหน้าหลัก</a>
 </div>
 </body>
 </html>
