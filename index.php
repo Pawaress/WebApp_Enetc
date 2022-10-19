ce1f001df23a608936807d89830ce7a8653c0270
Max <hs2max@gmail.com>
2022-10-19 12:04:16 +0700

    add all

--- index.php
+++ index.php
@@ -1,40 +1,50 @@
-<?php session_start(); 
-
-?>
+<?php session_start();?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    
+    <title>Document</title>   
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">  
 </head>
     <?php 
     if(!isset($_SESSION['id']))
-    { 
-    
+    {   
     ?>
 <body>
-    
+    <div class="container">
 
-    <h1 style="text-align:center ;">Webbord KakKak</h1><hr>
-    <div style="float:right ;"><a  href = "login.php">เข้าสู่ระบบ</a></div>
+    <h1 style="text-align:center ;">Webbord KakKak</h1>
+
+    <?php include "nav.php";?>
+   
+   
     <form action="post.php"method="get">
-    <form >
-        หมวดหมู่:
-    <select>
-        :
-        <option value="all">--ทั้งหมด--</option>
-        <option value="general">เรื่องทั่วไป</option>
-        <option value="study">เรื่องเรียน</option>
-    </select>
-</form>
-    
+   <br>
+   <div class="d-flex">
+        <div>
+            <label >หมวดหมู่</label>
+            <span class="dropdown">
+                <button class="btn btn-light dropdown-toggle btn-sm" type="button"
+                 id="button2" data-bs-toggle="dropdown" aria-expanded="false">--ทั้งหมด-- 
+                    <ul class="dropdown-menu" aria-labelledby="button2">
+                        <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+                        <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+                        <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
+                    </ul>
+                </button>
+            </span>
+        </div>
+
+   </div>
     <br>
-    <ul>
-
-
+    <table class="table table-striped">
+        
     <?php
     
         /*for($i=1;$i<=10;$i++){
@@ -43,42 +53,54 @@
 
         $i=1;
         do{
-            echo "<li><a href = post.php?id=$i>กระทู้ที่ $i</a></li>";
+            echo "<tr><td><a href = post.php?id=$i style=text-decoration:none>กระทู้ที่ $i</a></td></tr>";
             $i++;
         }while($i<=10);
 
     /* แบบเก่า  <li><a href = "post.php?id=1">กระทู้ที่ 1</a></li>  */
     ?>
+
+    </table>
+    <br>
+    <ul>
+
+
+  
      
     </ul>
 </form>
+</div>
 </body>
 
 <?php
     }else{
 ?>
 <body>
-    
+<div class="container">
 
-    <h1 style="text-align:center ;">Webbord KakKak</h1><hr>
+    <h1 style="text-align:center ;">Webbord KakKak</h1>
+    <?php include "nav.php";?>
+    <hr>
     
     <form action="post.php"method="get">
    
-        หมวดหมู่:
-    <select>
-        :
-        <option value="all">--ทั้งหมด--</option>
-        <option value="general">เรื่องทั่วไป</option>
-        <option value="study">เรื่องเรียน</option>
-    </select>
-        <div style="float:right">
-        
-        <?php
-            
-            echo "ผู้ใช้งานระบบ : $_SESSION[username]";
-        ?>&nbsp;&nbsp;
-            <a  href = "logout.php">  ออกจากระบบ</a>
+    <div class="d-flex">
+        <div>
+            <label >หมวดหมู่</label>
+            <span class="dropdown">
+                <button class="btn btn-light dropdown-toggle btn-sm" type="button"
+                 id="button2" data-bs-toggle="dropdown" aria-expanded="false">--ทั้งหมด-- 
+                    <ul class="dropdown-menu" aria-labelledby="button2">
+                        <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+                        <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+                        <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
+                    </ul>
+                </button>
+            </span>
         </div>
+
+   </div>
+       
          
         
     
@@ -109,6 +131,7 @@
      
     </ul>
 </form>
+</div>
 </body>
 
 <?php
